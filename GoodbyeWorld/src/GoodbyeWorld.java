b5c2abedbf1da01fe8f28f0836d7b48218d27ee6
ab00179 <ab00179@BNE-7WL0853.wvu-ad.wvu.edu>
2023-08-24 12:32:57 -0400

    Added a GoodByeWorld Project to the repo. I am feeling confused.

---
+++ GoodbyeWorld/src/GoodbyeWorld.java
@@ -0,0 +1,19 @@
+/** Author: Ayah-mounina Bouzihay
+ * Date:8/24/2023
+ * This is juat a trial file
+ * This program prints out the name of my pets.
+ * 
+ */
+public class GoodbyeWorld {
+	public static void main(String[] args) {
+		//Print out a message to users
+		System.out.println("This is a message to display to users");
+		
+		//Print out an other message to users
+		System.out.println("This is a message to display to users");
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
